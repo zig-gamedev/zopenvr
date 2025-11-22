d6f20b7739e47dff712eac2aa5b63971b8786e12
hazeycode <22148308+hazeycode@users.noreply.github.com>
2025-11-22 19:21:54 +0000

    git lfs stuff

--- build.zig
+++ build.zig
@@ -1,6 +1,13 @@
 const std = @import("std");
 
-pub fn build(b: *std.Build) void {
+pub fn build(b: *std.Build) !void {
+    checkGitLfsContent() catch {
+        try ensureGit(b.allocator);
+        try ensureGitLfs(b.allocator, "install");
+        try ensureGitLfs(b.allocator, "pull");
+        try checkGitLfsContent();
+    };
+
     const zopenvr = b.addModule("root", .{
         .root_source_file = b.path("src/openvr.zig"),
     });
@@ -118,5 +125,83 @@ pub fn installOpenVR(
             }
         },
         else => {},
+    }
+}
+
+fn ensureGit(allocator: std.mem.Allocator) !void {
+    const printErrorMsg = (struct {
+        fn impl() void {
+            std.log.err("\n" ++
+                \\---------------------------------------------------------------------------
+                \\
+                \\'git version' failed. Is Git not installed?
+                \\
+                \\---------------------------------------------------------------------------
+                \\
+            , .{});
+        }
+    }).impl;
+    const argv = &[_][]const u8{ "git", "version" };
+    const result = std.process.Child.run(.{
+        .allocator = allocator,
+        .argv = argv,
+    }) catch { // e.g. FileNotFound
+        printErrorMsg();
+        return error.GitNotFound;
+    };
+    defer {
+        allocator.free(result.stderr);
+        allocator.free(result.stdout);
+    }
+    if (result.term.Exited != 0) {
+        printErrorMsg();
+        return error.GitNotFound;
+    }
+}
+
+fn ensureGitLfs(allocator: std.mem.Allocator, cmd: []const u8) !void {
+    const printNoGitLfs = (struct {
+        fn impl() void {
+            std.log.err("\n" ++
+                \\---------------------------------------------------------------------------
+                \\
+                \\Please install Git LFS (Large File Support) extension and run 'zig build' again.
+                \\
+                \\For more info about Git LFS see: https://git-lfs.github.com/
+                \\
+                \\---------------------------------------------------------------------------
+                \\
+            , .{});
+        }
+    }).impl;
+    const argv = &[_][]const u8{ "git", "lfs", cmd };
+    const result = std.process.Child.run(.{
+        .allocator = allocator,
+        .argv = argv,
+    }) catch { // e.g. FileNotFound
+        printNoGitLfs();
+        return error.GitLfsNotFound;
+    };
+    defer {
+        allocator.free(result.stderr);
+        allocator.free(result.stdout);
+    }
+    if (result.term.Exited != 0) {
+        printNoGitLfs();
+        return error.GitLfsNotFound;
+    }
+}
+
+fn checkGitLfsContent() !void {
+    const expected_contents =
+        \\DO NOT EDIT OR DELETE
+        \\This file is used to check if Git LFS content has been downloaded
+    ;
+    var buf: [expected_contents.len]u8 = undefined;
+    _ = std.fs.cwd().readFile(".lfs-content-token", &buf) catch {
+        return error.GitLfsContentTokenNotFound;
+    };
+    if (!std.mem.eql(u8, expected_contents, &buf)) {
+        return error.GitLfsContentCheckFailed;
     }
 }
